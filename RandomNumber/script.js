30e15d6224348f3218ce767df2fd5d8e5430981f
NishaAligithub <168262875+NishaAligithub@users.noreply.github.com>
2025-06-26 14:49:35 +0500

    created a dice with html,css,js
    made a dice on clicking roll generates a random number between 1 to 6 as (dice in ludo game)

---
+++ RandomNumber/script.js
@@ -0,0 +1,11 @@
+const btn=document.getElementById("btn");
+const myLabel=document.getElementById("myLabel");
+const min=1;
+const max=6;
+
+let randomNumber;
+btn.onclick=function(){
+    randomNumber=Math.floor(Math.random()*max)+min;
+    myLabel.textContent=randomNumber;
+    myLabel2.textContent=`Yayyyyy!you have got ${randomNumber} `;
+}

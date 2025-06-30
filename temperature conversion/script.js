8848f8068bd28cd13c3c09023933db428a12c120
Nisha Ali <168262875+NishaAligithub@users.noreply.github.com>
2025-06-30 16:43:43 +0500

    project:Temperature converter by html,css and js
     just created Temperature converter by using html,css and ..

---
+++ temperature conversion/script.js
@@ -0,0 +1,28 @@
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius"); 
+const result = document.getElementById("result");
+let temperature;
+
+
+
+
+function convert(){
+    if(toFahrenheit.checked){
+        temperature=Number(textBox.value);
+        temperature = (temperature * 9/5) + 32;
+         result.textContent = `${temperature.toFixed(1)}°F`;
+
+       
+
+    }else if(toCelsius.checked){
+        temperature=Number(textBox.value);
+        temperature = (temperature - 32) * 5/9;
+        result.textContent = `${temperature.toFixed(1)}°C`;
+        
+
+}else
+{
+        result.textContent = "Please select a conversion option.";
+    }
+}
